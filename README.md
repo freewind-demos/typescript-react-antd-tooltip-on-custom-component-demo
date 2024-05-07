098d7b79ae0db4b11a270367b3fa726dc7c6a04c
Freewind <nowindlee@gmail.com>
2024-05-07 18:28:26 +0800

    fix

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ TypeScript React AntD Tooltip on Custom Component Demo
 
 Not sure why the "Tooltip" component doesn't show on a custom component
 
+https://github.com/ant-design/ant-design/issues/15909
+
+Tooltip需要向子组件中插入一些额外属性，我们必须让子组件可以接受
+
 ```
 npm install
 npm start
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,13 +1,18 @@
 import { Space, Tooltip } from 'antd';
 import React, { FC } from 'react';
 
-const MyText: FC = ({ children }) => {
+const MyBadWrapper: FC = ({ children }) => {
   return <span style={{ border: '1px solid red' }}>{children}</span>
 }
 
+// NOTE have to accept extra props from the outer and pass into inner component
+const MyGoodWrapper: FC = ({ children, ...rest }) => {
+  return <span style={{ border: '1px solid red' }} {...rest}>{children}</span>
+}
 export default function Hello() {
   return <Space>
     <Tooltip title="hello"><span style={{ border: '1px solid red' }}>Good Tooltip</span></Tooltip>
-    <Tooltip title="hello"><MyText>No Tooltip</MyText></Tooltip>
+    <Tooltip title="hello"><MyGoodWrapper>Good Custom Tooltip</MyGoodWrapper></Tooltip>
+    <Tooltip title="hello"><MyBadWrapper>No Tooltip</MyBadWrapper></Tooltip>
   </Space>
 };
